57bfbe83270368294d7da58746652161cfb287a2
lairaalmas <lairaalmas@gmail.com>
2022-09-01 23:22:58 -0300

    create interactive timer

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -18,13 +18,23 @@ export default function Cronometro({ selecionado }: Props) {
     }
   }, [selecionado]);
 
+  // se contador for undefined coloca 0
+  function regressiva(contador: number = 0) {
+    setTimeout(() => {
+      if (contador > 0) {
+        setTempo(contador - 1);
+        return regressiva(contador - 1);
+      }
+    }, 1000);
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
         <Relogio tempo={tempo} />
       </div>
-      <Botao>Começar</Botao>
+      <Botao onClick={() => regressiva(tempo)}>Começar</Botao>
     </div>
   );
 }
